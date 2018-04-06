28aa540eb09b18bcace978053030db49535ba5ee
Nivedit <niveditphoenix@gmail.com>
2018-04-06 19:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ def profile(name):
 	#new_name = name + " is awesome..! "
 	return render_template('index.html', name=name)
 
+#@app.route('/add_numbers', methods=['GET','POST'])
+#def add_numbers_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+	  #if request.method == 'GET':
+	  #	return render_template('add_numbers.html')
+	  #elif request.method == 'POST':
+  	  #    print(request.form['text'].split())
+  	  #    total = 0
+  	  #    try:
+  	  #    	for str_num in request.form['text'].split():
+  	  #    		total += int(str_num)*int(str_num)
+  	  #    	return render_template('add_numbers.html', result=str(total))
+  	  #    except ValueError:
+  	  #    	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
 @app.route('/add_numbers', methods=['GET','POST'])
 def add_numbers_post():
@@ -27,14 +42,18 @@ def add_numbers_post():
 	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
-  	      total = 0
+  	      #total = 0
+		  text_list = request.form['text'].split()
+		  text_rev_list = []
   	      try:
-  	      	for str_num in request.form['text'].split():
-  	      		total += int(str_num)*int(str_num)
-  	      	return render_template('add_numbers.html', result=str(total))
+  	      	#for str_num in request.form['text'].split():
+			for i in range(len(text_list)-1,-1,-1):
+  	      		#total += int(str_num)*int(str_num)
+				text_rev_list.append(text_list[i])
+  	      	#return render_template('add_numbers.html', result=str(total))
+			return render_template('add_numbers.html', result=text_rev_list)
   	      except ValueError:
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
-
 
 @app.route('/shopping_list', methods=['GET','POST'])
 def shopping_list_post():
